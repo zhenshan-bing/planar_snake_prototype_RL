817afedcaaa17534e9704f4992870c2928a79f5a
BZSROCKETS <bing@in.tum.de>
2021-08-09 21:54:25 +0200

    update README

--- README.md
+++ README.md
@@ -19,3 +19,19 @@ source ./planar_snake_prototype_env/bin/activate
 
 #### 3. Install mujoco_py
 Please follow the instructions at [https://github.com/openai/mujoco-py](https://github.com/openai/mujoco-py)
+Please check if you have successfully installed mujoco_py by checking
+```bash 
+python3
+import mujoco_py
+```
+
+#### 3. Install Planar Snake 
+```bash 
+git clone https://github.com/BZSROCKETS/planar_snake_prototype_RL.git
+cd planar_snake_prototype_RL
+pip install -e .
+```
+### Testing
+```bash 
+python planar_snake_prototype_RL/planar_snake_prototype_RL/agents/run_mujoco_ppo.py
+```
--- setup.py
+++ setup.py
@@ -4,13 +4,16 @@ setup(name='planar-snake-prototype-RL',
       version='0.1.0',
       author="Zhenshan Bing",
       install_requires=['numpy>=1.10.4',
-                        'gym[mujoco,classic_control]',
                         #'mujoco',
                         #'mujoco-py<1.50.2,>=1.50.1',
-                        #'gym[mujoco]==0.9.6',
+                        'gym==0.11.0',
                         'glfw>=1.4.0',
                         'Cython>=0.27.2',
-                        #'baselgitines',
-                        #'skimage'
+                        'baselines',
+                        'tensorflow==1.14.0',
+                        'sklearn',
+                        'pandas',
+                        'matplotlib',
+                        'scikit-image'
                         ]
 ) 
